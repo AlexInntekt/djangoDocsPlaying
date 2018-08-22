47ad1b186c7f33de746feb602e9f158c5f0bc8ec
Manolescu Mihai Alexandru <alex.inntekt@gmail.com>
2018-08-22 12:04:58 +0300

    transfering specific data members from model into the context of the view (in this case, the date of the post)

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,14 @@ from .models import Post
 from django.utils import timezone
 
 def post_list(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
-	return render(request, 'blog/post_list.html', {'posts': posts})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def display(request):
+    posts = Post.objects.all()
+    dates = [posts.first().published_date]
+    dates.pop(0)
+
+    for post in posts:
+       dates.append(post.published_date)
+    return render(request, 'blog/display.html', {'dates': dates})
