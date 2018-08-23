37a98b9b00d7616a79c112e04c500bdf9657f695
Manolescu Mihai Alexandru <alex.inntekt@gmail.com>
2018-08-23 11:35:35 +0300

    I made some amazing changes in the default admin dashboard! The admin dashboard can be easily customized by changing the admin.py file and by creating a custom displaying class.

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import Post, Group
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+	fieldsets = [
+	  ('Membership',{'fields':['group']}),
+	  ('Basic information',{'fields':['author']}),
+      (None,{'fields':['title']}),
+      (None,{'fields':['text']}),
+      ('Date details',{'fields':['created_date']}),
+      (None,{'fields':['published_date']}),
+	]
+
+
+admin.site.register(Post,PostAdmin)
 admin.site.register(Group)
