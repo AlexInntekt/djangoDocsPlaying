5a63af0dbfd52d310b408822f383ff7e5ffb63bf
Manolescu Mihai Alexandru <alex.inntekt@gmail.com>
2018-08-23 12:09:18 +0300

    I added detailed information in the list of post objects in the dashboard

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Post, Group
 
+class PostInline(admin.TabularInline):
+    model = Post
+    extra = 1
 
 class PostAdmin(admin.ModelAdmin):
 	fieldsets = [
@@ -12,10 +15,14 @@ class PostAdmin(admin.ModelAdmin):
       (None,{'fields':['published_date']}),
 	]
 
+	list_display = ('title', 'author' ,'created_date', 'group')
+
 class GroupAdmin(admin.ModelAdmin):
 	fieldsets = [
        ('Basic information',{'fields':['name']}),
 	]
+	inlines = [PostInline]
+
 
 admin.site.register(Post,PostAdmin)
 admin.site.register(Group, GroupAdmin)
