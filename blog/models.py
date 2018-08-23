1bfff00b2ca7fd25454ecca537f69f5ba6099668
Manolescu Mihai Alexandru <alex.inntekt@gmail.com>
2018-08-23 10:46:53 +0300

    Added reverse access. Reverse access throug: Group.objects.all().first().post_set.all().first().title

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,11 @@
 from django.db import models
 from django.utils import timezone
 
+class Group(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
@@ -11,6 +16,8 @@ class Post(models.Model):
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
+
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, null=True, blank=True)
 
     def publish(self):
         self.published_date = timezone.now()
